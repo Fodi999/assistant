7f4b729fb3a11ae1e0471c1eb9ba2741de50a144
Dmytro Fomin <fodi85999@gmail.com>
2026-02-09 16:57:22 +0100

    feat: add avocado image URL to catalog

---
+++ migrations/20240113000001_add_avocado_image.sql
@@ -0,0 +1,4 @@
+-- Add image URL to avocado in catalog
+UPDATE catalog_ingredients
+SET image_url = 'https://i.postimg.cc/KjfqhLX2/fodifood-single-whole-avocado-top-view-flat-lay-food-photograph-f701dbb9-1b31-4d0b-99ce-96f16a2c413d.png'
+WHERE name_en = 'Avocado';
