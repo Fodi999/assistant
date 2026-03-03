e68ad7870b103c775905bcd4d464de2c24282989
Dmytro Fomin <fodi85999@gmail.com>
2026-03-03 15:53:49 +0100

    fix: remove markdown code fence from SQL migration (caused PgDatabaseError syntax error)

--- migrations/20260224000002_add_image_url_to_dishes.sql
+++ migrations/20260224000002_add_image_url_to_dishes.sql
@@ -1,17 +1,3 @@
-```sql
 -- Add image_url to dishes table
-
--- +goose Up
--- +goose StatementBegin
 ALTER TABLE dishes
 ADD COLUMN IF NOT EXISTS image_url TEXT;
-
-COMMENT ON COLUMN dishes.image_url IS 'URL of the dish image (optional)';
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE dishes
-DROP COLUMN IF EXISTS image_url;
--- +goose StatementEnd
-```
